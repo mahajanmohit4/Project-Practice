28e2ba642a364f7cfb480aeba40c7cab25d04d8d
mahajanmohit4 <mahajanmohit4@gmail.com>
2021-07-02 16:12:10 +0530

    new

--- src/app/pages/draggable/draggable.module.ts
+++ src/app/pages/draggable/draggable.module.ts
@@ -3,13 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { DraggableRoutingModule } from './draggable-routing.module';
 import { DraggableComponent } from './draggable/draggable.component';
-
+import { MaterialModule } from 'src/app/layout/material/material.module';
+import { AngularDraggableModule } from 'angular2-draggable';
+import { NgDragDropModule } from 'ng-drag-drop';
+import { AutosizeModule } from 'ngx-autosize';
 
 @NgModule({
   declarations: [DraggableComponent],
   imports: [
     CommonModule,
-    DraggableRoutingModule
-  ]
+    DraggableRoutingModule,
+    MaterialModule,
+    AngularDraggableModule,
+    AutosizeModule,
+    NgDragDropModule,
+  ],
+  exports: [DraggableComponent],
 })
-export class DraggableModule { }
+export class DraggableModule {}
--- src/app/pages/draggable/draggable/draggable.component.ts
+++ src/app/pages/draggable/draggable/draggable.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
+declare const require: any;
 @Component({
   selector: 'app-draggable',
   templateUrl: './draggable.component.html',
-  styleUrls: ['./draggable.component.scss']
+  styleUrls: ['./draggable.component.scss'],
 })
 export class DraggableComponent implements OnInit {
+  @HostListener('document:mousedown', ['$event'])
+  demo_html = require('!!html-loader!./draggable.component.html');
+  demo_ts = require('!!raw-loader!./draggable.component.ts');
+  demo_scss = require('!!raw-loader!./draggable.component.scss');
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
