5136b3672a4222de7654a9a6c62891c06b228426
mahajanmohit4 <mahajanmohit4@gmail.com>
2021-07-04 15:47:38 +0530

    new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PravinSethComponent } from './pravin-seth/pravin-seth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PravinSethComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pravin-seth/pravin-seth.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pravin-seth',
+  templateUrl: './pravin-seth.component.html',
+  styleUrls: ['./pravin-seth.component.scss']
+})
+export class PravinSethComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
